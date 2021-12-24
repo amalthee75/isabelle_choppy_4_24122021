41bcf51bddfe149a4798e8c6d1540c1788b0a8db
amalthee75 <64910055+amalthee75@users.noreply.github.com>
2021-12-24 13:10:53 +0100

    init

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog-ecrivain</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="general">
+       <header>
+           <div id="logo">
+               <div id="img"></div>
+               <h1>Blog de Jean Forteroche</h1>
+                <h2>Romans historiques - Essais</h2>
+           </div>
+           <nav>
+               <ul>
+                   <li><a href="#">Accueil</a> </li>
+                   <li><a href="#">Blog</a> </li>
+                   <li><a href="#">contact</a> </li>
+               </ul>
+           </nav>
+       </header>
+    </div>
+    <div id="banniere-image">
+        <div id="banniere-description">
+            Mes romans - Mes actualité
+            <a href="#">voir l'article</a>
+        </div>
+    </div>
+    <section>
+        <article>
+            <h1>Titre</h1>
+            <p>A l'heure où blanchit la campagne, je partirai<br>
+        Vois-tu, je sais que tu m'attends...</p>
+        </article>
+        <aside>
+            <h2>A propos de l'auteur</h2>
+            <p>Jean Forteroche est né en 1950. Ecrivain à succès reconnu</p>
+        </aside>
+
+    </section>
+
+    <footer>
+        <div id="mesphotos">
+            <a href="#">Mes photos</a>
+        </div>
+        <ul>
+            <li><a href="#">facebook</a></li>
+            <li><a href="#">instagram</a></li>
+            <li><a href="#">gettr</a></li>
+            <li><a href="#">telegram</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
